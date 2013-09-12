ce98e6522186ab75f60171e7dbea82ff3930d044
Breck <breck7@gmail.com>
2013-09-12 12:34:36 -0700

    updates

--- README.md
+++ README.md
@@ -1,4 +1,46 @@
-spaceful.org
-============
+Slides
+======
 
-Website for spaceful.org
+Slides is a [Space](https://github.com/nudgepad/space) based Slideshow language.
+
+Try it now
+==========
+
+Try it now at http://slides.nudgepad.com
+
+Examples
+========
+
+### Basic Example
+
+```
+<script src="slides.js"></script>
+<template id="slideshow" style="display: none;">
+options
+ title Slides
+slide
+ title Hello world
+ subtitle This is Slides
+ background rgb(26,134,214)
+slide
+ title A lightweight language for building Slideshows.
+ subtitle (Look at the source of this page.)
+ background rgb(36,65,112)
+slide
+ title It uses Bootstrap, jQuery, and Space
+ subtitle Open source FTW
+ background rgb(231,189,44)
+slide
+ title Your slides can have background colors...
+ background rgb(71,41,54)
+slide
+ title ...or background images
+ image starrynight.jpg
+slide
+ title What do you think?
+ subtitle Email me <a style="color:white;" href="mailto:breck7@gmail.com">breck7@gmail.com</a> 
+ color green
+ background rgb(224,54,52)
+</template>
+```
+
--- slides.js
+++ slides.js
@@ -1,25 +1,83 @@
-var Slides = {}
-
-Slides.Slide = function () {
-    
-  this.clear()
-
-  if (space)
-    this.patch(space)
+/**
+ * Insert a script into the DOM
+ *
+ * @param {string} URL of script
+ * @param {function} Optional callback
+ */
+var AppendScript = function (src, callback) {
+  var script = document.createElement( 'script' )
+  script.type = 'text/javascript'
+  script.src = src
+  if (callback)
+    script.onload = callback
+  document.getElementsByTagName('head')[0].appendChild(script)
 }
 
-Slides.Slide.prototype = new Space()
+AppendScript('bootstrap/js/jquery-1.10.2.min.js', function () {
+  AppendScript('bootstrap/js/bootstrap.min.js', function () {
+    AppendScript('space.js', function () {
+      $('body').append('<meta name="viewport" content="width=device-width, initial-scale=1.0">')
+      $('body').append('<link href="bootstrap/css/bootstrap.css" rel="stylesheet">')
+      $('body').append('<link href="style.css" rel="stylesheet">')
+      $('body').append(template)
+      var slides = new Space($('#slideshow').html())
+      var i = 0
+      if (slides.get('options title'))
+        $('body').append('<title>' + slides.get('options title') + '</title>')
+      slides.each(function (key, value) {
+        if (key !== 'slide')
+          return true
+        $('.carousel-indicators').append('<li data-target="#carousel-example-generic" data-slide-to="' + i + '"></li>')
+        var slide = $('<div class="item"></div>')
+        var caption = $('<div class="carousel-caption"></div>')
+        if (value.get('title'))
+          caption.append('<h3>' + value.get('title') + '</h3>')
+        if (value.get('subtitle'))
+          caption.append('<p>' + value.get('subtitle') + '</p>')
+        slide.append(caption)
+        if (value.get('image'))
+          slide.css('background-image', 'url(' + value.get('image') + ')')
+        
+        if (value.get('background'))
+          slide.css('background-color', value.get('background'))
 
-Slides.Show = function () {
-    
-  this.clear()
+        if (value.get('color'))
+          slide.css('color', value.get('color'))
+        
+        $('.carousel-inner').append(slide)
+        
+        i++
+      })
+      $($('.carousel-indicators')[0]).addClass('active')
+      $($('.item')[0]).addClass('active')
+      $('.carousel').show()
+      var options = {}
+      if (slides.get('options interval') === false)
+        options.interval = false
+      else if (slides.get('options interval'))
+        options.interval = parseFloat(slides.get('options interval'))
+      $('.carousel').carousel(options)
+      console.log('hi')
+    })
+  })
+})
 
-  if (space)
-    this.patch(space)
-}
 
-Slides.Show.prototype = new Space()
+var template = '<div id="carousel-example-generic" class="carousel slide" style="display: none;">\
+  <!-- Indicators -->\
+  <ol class="carousel-indicators">\
+  </ol>\
+\
+  <!-- Wrapper for slides -->\
+  <div class="carousel-inner">\
+  </div>\
+\
+  <!-- Controls -->\
+  <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">\
+    <span class="icon-prev"></span>\
+  </a>\
+  <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">\
+    <span class="icon-next"></span>\
+  </a>\
+</div>'
 
-Slides.Show.prototype.toHtml = function () {
-    
-}
